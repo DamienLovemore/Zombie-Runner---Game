aa6a840c52f032e287ce48b294ed302bad5fd86e
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-12-14 11:36:52 -0300

    Enemies now follow the player only when it gets close enough

--- Assets/Resources/Scripts/EnemyAI.cs
+++ Assets/Resources/Scripts/EnemyAI.cs
@@ -6,7 +6,12 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private float chaseRange = 5f;
+
     private NavMeshAgent navMeshAgent;
+    //Stats with the enemy being away from the player
+    //(To prevent enemies from chasing the player in the start)
+    private float distanceToTarget = Mathf.Infinity;
    
     void Start()
     {
@@ -15,8 +20,13 @@ public class EnemyAI : MonoBehaviour
     
     void Update()
     {
-        //Makes the enemy AI follow the target position
-        //(The player for example)
-        this.navMeshAgent.SetDestination(this.target.position);
+        //Calculates the distance between the enemy and the target
+        distanceToTarget = Vector3.Distance(target.position, this.transform.position);
+
+        //Begins chasing the target if it gets close enough
+        if(distanceToTarget <= this.chaseRange)
+            //Makes the enemy AI follow the target position
+            //(The player for example)
+            this.navMeshAgent.SetDestination(this.target.position);
     }
 }
