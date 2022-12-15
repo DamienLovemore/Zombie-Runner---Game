c9325478817a535c716341bfd6fd80c8d97c1236
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-12-15 13:48:13 -0300

    VFX for when the bullet hits something was added

--- Assets/Resources/Scripts/Weapon.cs
+++ Assets/Resources/Scripts/Weapon.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [Header("Aim target")]
     [SerializeField] private Camera FPCamera;
+
+    [Header("Weapon config")]
     [SerializeField] private float bulletRange = 100f;
     [SerializeField] private float damage = 30f;
+
+    [Header("VFXs")]
     [SerializeField] private ParticleSystem muzzleFlash;
+    [SerializeField] private GameObject hitEffect;
 
     private void OnFire()
     {
@@ -35,6 +41,8 @@ public class Weapon : MonoBehaviour
         //Verifies if it has hitted something or not
         if (hittedSomething)
         {
+            this.CreateHitImpact(hit);
+
             //Verifies it has hitted an enemy (not a object)
             EnemyHealth targetHealth = hit.transform.GetComponent<EnemyHealth>();
             if (targetHealth != null)
@@ -43,5 +51,15 @@ public class Weapon : MonoBehaviour
                 targetHealth.TakeDamage(this.damage);
             }
         }
+    }
+
+    //Play a hit impact effect where the bullet hitted
+    //(If it has hitted something)
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        //Instanties the hit effect on the target
+        GameObject hitEffect = Instantiate(this.hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        //Destroys it after it has been shown
+        Destroy(hitEffect, 0.1f);
     }
 }
