fecf5980608551f85a0357b05e44feb8f3391b93
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-12-21 08:31:26 -0300

    Final build version and bug fixes

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,12 +9,49 @@ public class GameManager : MonoBehaviour
     void Start()
     {   
         this.sceneLoader = FindObjectOfType<SceneLoader>();
-    }
+    }    
 
     void LateUpdate()
     {
         //Verifies if the player has achieved the goal
         this.VerifyWinningCondition();
+    }
+
+    //Process shortcuts hotkey like F11 and Esc keys
+    private void OnProcessShortcuts()
+    {
+        //Quits the game
+        if(Keyboard.current.escapeKey.isPressed)
+        {
+            this.QuitGame();
+        }
+        //Toggle window mode (fullscreen or windowed)
+        else if (Keyboard.current.f11Key.isPressed)
+        {
+            this.ToggleWindowMode();
+        }
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void ToggleWindowMode()
+    {
+        //Gets the current window mode
+        FullScreenMode currentScreenMode = Screen.fullScreenMode;
+        
+        //If it is in Fullscreen mode, then switch to windowed mode
+        if((currentScreenMode != FullScreenMode.Windowed) && (currentScreenMode != FullScreenMode.MaximizedWindow))
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+        }
+        //If it is in windowed mode, then switch to fullscreen mode
+        else
+        {
+            Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+        }
     }
 
     //Verifies if there is no more enemies alive, then throws
